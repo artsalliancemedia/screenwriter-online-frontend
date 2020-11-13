f640619e1c18d9eb056e651bd8166e7457b33801
toobe <toobe.loong@gmail.com>
2020-11-13 14:23:10 +0800

    -

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,31 +1,28 @@
 import { register } from 'register-service-worker';
 
-console.log(process.env.BASE_URL);
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB'
-      );
+    registrationOptions: { scope: './' },
+    ready (registration) {
+      console.log('Service worker is active.')
     },
-    registered () {
-      console.log('Service worker has been registered.');
+    registered (registration) {
+      console.log('Service worker has been registered.')
     },
-    cached () {
-      console.log('Content has been cached for offline use.');
+    cached (registration) {
+      console.log('Content has been cached for offline use.')
     },
-    updatefound () {
-      console.log('New content is downloading.');
+    updatefound (registration) {
+      console.log('New content is downloading.')
     },
-    updated () {
-      console.log('New content is available; please refresh.');
+    updated (registration) {
+      console.log('New content is available; please refresh.')
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
-      console.error('Error during service worker registration:', error);
+      console.error('Error during service worker registration:', error)
     }
   });
 }
